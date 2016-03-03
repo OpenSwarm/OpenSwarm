5065fab1736be8d8ef3f2a5850dd29fbf6b2b5a0
Stefan M. Trenkwalder <stefan@trenkwalder.tech>
2016-03-03 23:28:37 +0000

    Created an didactic main for Roderich's lab ACS6334

--- main.c
+++ main.c
@@ -42,9 +42,6 @@
 /* Main Program                                                               */
 /******************************************************************************/
 
-static uint fps = 0;
-
-void loggingThread();
 void thread1();
 void thread2();
 void thread3();
@@ -53,40 +50,12 @@ void thread5();
 void thread6();
 void thread7();
 void log_me();
-bool wait100times(void *data);
-bool wait250times(void *data);
-bool wait1000times(void *data);
-bool object_clustering(uint16 PID, uint16 EventID, sys_event_data *data);
-bool logging(uint16 PID, uint16 EventID, sys_event_data *data);
-bool toggleLED(uint16 PID, uint16 EventID, sys_event_data *data);
+bool toggle_frontLED(uint16 PID, uint16 EventID, sys_event_data *data);
+bool toggle_frontLED2(uint16 PID, uint16 EventID, sys_event_data *data);
 
 int16_t main(void)
 {
-
-    //Sys_Init_MCU_HDI();
-    /* Configure the oscillator for the device */
-    Sys_Init_Kernel();
-    
-//    Sys_SetReadingFunction_UART1(bluetooth_reader);
-    
-    if(   !Sys_Start_Process(thread1) ||
-            !Sys_Start_Process(thread2) ||
-            !Sys_Start_Process(thread3) ||
-            !Sys_Start_Process(thread4) ||
-            !Sys_Start_Process(thread5) ||
-            !Sys_Start_Process(thread6) ||
-            !Sys_Start_Process(thread7)
-      ){
-        FRONT_LED = 1;
-    }
-   
-    //Sys_Subscribe_to_Event(SYS_EVENT_1ms_CLOCK, 0, logging, wait1000times);//once per second
-//    Sys_Subscribe_to_Event(SYS_EVENT_1ms_CLOCK, 0, toggleLED, wait1000times);//once per second
-    
-    Sys_Subscribe_to_Event(SYS_EVENT_IO_CAMERA, 0, object_clustering, 0);
-    
-    Sys_Start_Kernel();
-
+    //turn all LEDs off
     LED0 = 0;
     LED1 = 0;
     LED2 = 0;
@@ -98,193 +67,66 @@ int16_t main(void)
     BODY_LED = 0;
     FRONT_LED = 0;
     
-    //sys_event_data * data = Sys_Wait_For_Event(SYS_EVENT_TERMINATION);
-    //Sys_Clear_EventData(&data);
-      
+    Sys_Init_Kernel(); //initialise OpenSwarm
     
+    if(   !Sys_Start_Process(thread1) ||
+          !Sys_Start_Process(thread2) ||
+          !Sys_Start_Process(thread3) ||
+          !Sys_Start_Process(thread4) ||
+          !Sys_Start_Process(thread5) ||
+          !Sys_Start_Process(thread6) ||
+          !Sys_Start_Process(thread7)
+      ){//start 7 extra threads. If an error occurred -> turn on the FRONT_LED
+        FRONT_LED = 1;
+    }
+       
+    //execute toggle_frontLED every time when the selector was changed
+    Sys_Subscribe_to_Event(SYS_EVENT_IO_SELECTOR_CHANGE, 0, toggle_frontLED, 0);
+    
+    Sys_Start_Kernel();//start OpenSwarm -> From now on all threads are running concurrently
+      
     Sys_Writeto_UART1("OS Started\r\n", 12);//send via Bluetooth
     
-    int i = 0;  
-    sint speed = 0;
-    sint inc = 1;
-    uint32 time = Sys_Get_SystemClock();
-    time += (uint32) 1000;
-    while(true){//DO Nothing (do yonly things for testing)
+    uint32 time = 1000;
+    while(true){//DO Nothing (do only things for testing)
         
-        
-        if(i == 0xFFFE){
-            i = 0;
-         
-            if(speed >= MAX_WHEEL_SPEED_MM_S){
-                inc = -8;
-            }
-        
-            if(speed <= -MAX_WHEEL_SPEED_MM_S){
-                inc = 8;
-            }
-            
-            speed += inc;
-            
-            Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_LEFT, speed);
-            Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_RIGHT, -speed);
-            LED0 = ~LED0; 
-            
-              
-//    static char message[24];
-//    uint16 length = 0;
-//    length = sprintf(message, "p: %u\r\n", Sys_Get_Number_Processes());
-//    Sys_Writeto_UART1(message, length);//send via Bluetooth
-//    
+        uint32 time_now = Sys_Get_SystemClock();//get the current time
+        if(time_now >= time){//if one second passed
+            time += 1000;//calculate next second
+            LED0 = ~LED0;//toggle LED0
         }
-        
-        uint32 time_now = Sys_Get_SystemClock();
-        if(time_now >= time){
-            time += 1000;
-            log_me();
-            LED2 = ~LED2;
-        }
-        
-        i++;
     }
 }
 
-/*
-bool remotecontrol_reader(uint16 pid, uint16 eventID, sys_event_data *data){
-    char msg[24] = {0};
-    uint8 length = 0;
-    uint8 *value = data->value;
-    length = sprintf(msg, "rc:%u\r\n", (int) value[0] );
-    Sys_Writeto_UART1(msg, length);
-
-    
-    run_clustering = false;
-    sint16 speed = 0;
-    
-    switch(*value){
-        case RC_BUTTON_SWAP:
-            srand(Sys_Get_SystemClock() & 0xFF);
-            speed = (rand() % (2*MAX_WHEEL_SPEED_MM_S))-MAX_WHEEL_SPEED_MM_S;//speed \in [-MAX_WHEEL_SPEED_MM_S, MAX_WHEEL_SPEED_MM_S]
-            Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_LEFT, speed);
-            Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_RIGHT, -speed);
-            BODY_LED = 1;
-            FRONT_LED = 0;
-            break;
-        case RC_BUTTON_OK:
-            speed = 0;
-            BODY_LED = 0;
-            FRONT_LED = 0;
-            Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_LEFT, 0);
-            Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_RIGHT, 0);
-            run_clustering = true;
-            break;
-        case RC_BUTTON_STANDBY:
-        case RC_BUTTON_SLEEP:
-            speed = 0;
-            BODY_LED = 0;
-            FRONT_LED = 0;
-            Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_LEFT, 0);
-            Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_RIGHT, 0);
-        default:
-            break;
-    }
-    return true;
-}
-
-*/
-void bluetooth_reader(uint8 data){    
-   // Sys_Writeto_UART1(&data,1);
-    ;
-}
-
-#define ROBOT_SPEED_L   (MAX_WHEEL_SPEED_MM_S * 89)/100
-#define ROBOT_SPEED_R   (MAX_WHEEL_SPEED_MM_S * 53)/100
-
-#define OBJECT_SPEED_L  (MAX_WHEEL_SPEED_MM_S * 98)/100
-#define OBJECT_SPEED_R  (MAX_WHEEL_SPEED_MM_S * 50)/100
-
-#define NOTHING_SPEED_L (MAX_WHEEL_SPEED_MM_S * 55)/100
-#define NOTHING_SPEED_R (MAX_WHEEL_SPEED_MM_S * 99)/100
-bool object_clustering(uint16 PID, uint16 EventID, sys_event_data *data){
-    BODY_LED = ~BODY_LED;  
-    fps++; 
-    /*if(!run_clustering){
-        return true;
-    }*/
-    
-    
-    sys_colour rx_colour = *((sys_colour *)data->value);
-   
-//    static char message[24];
-//    uint16 length = 0;
-    
-    if(rx_colour & 0x01){
-        LED3 = 1;
-    }else{
-        LED3 = 0;
-    }
-    
-    if(rx_colour & 0x02){
-        LED4 = 1;
-    }else{
-        LED4 = 0;
-    }
-    
-    if(rx_colour & 0x04){
-        LED5 = 1;
-    }else{
-        LED5 = 0;
-    }
-    //length = sprintf(message, "colour:%i <%i,%i,%i>\r\n", rx_colour, RED, GREEN, BLUE);
-    //Sys_Writeto_UART1(message, length);//send via Bluetooth
-    
-    /*
-    switch(rx_colour){//detection of
-    case GREEN://other robot
-        Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_LEFT, ROBOT_SPEED_L);
-        Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_RIGHT,ROBOT_SPEED_R);
-        LED5 = ~LED5;
-        break;
-    case RED://object
-        Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_LEFT, OBJECT_SPEED_L);
-        Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_RIGHT, OBJECT_SPEED_R);
-        LED6 = ~LED6;
-        break;
-    case WHITE://nothing/wall
-        Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_LEFT, NOTHING_SPEED_L);
-        Sys_Send_IntEvent(SYS_EVENT_IO_MOTOR_RIGHT, NOTHING_SPEED_R);
-        LED7 = ~LED7;
-        break;
-    default://anything else
-        LED1 = ~LED1;
-        break;//keep on
-    }
-    */
-    return true;
-}
-
+/**
+ * Here is an example how you can send data via Bluetooth to your PC
+ * 
+ */
 void log_me(){
-    static char message[30];
+    static char message[30];//your string
     
-    uint length = 0;
-    length = sprintf(message, "%07ld;%2u;%4u;%4u;%2u\r\n", Sys_Get_SystemTime(),// 
+    uint length = 0;//length of the string
+    length = sprintf(message, "%07ld;%2u;%4u;%4u\r\n", Sys_Get_SystemTime(),// 
                                                     Sys_Get_Number_Processes(),// 
                                                     Sys_Get_InterruptCounter(),//
-                                                    Sys_Get_EventCounter(), //
-                                                    fps);
+                                                    Sys_Get_EventCounter());//convert numbers into a string
     
     Sys_Writeto_UART1(message, length);//send via Bluetooth
     
     Sys_Reset_InterruptCounter();
     Sys_Reset_EventCounter();
-    fps = 0;
 }
 
+/**
+ * These functions represent 7 threads. Each thread toggles a different LED after the 65534th increment of i
+ * 
+ */
 void thread1(){
     uint i = 0;    
     while(true){//DO Nothing (do only things for testing)
         if(i == 0xFFFE){
             i = 0;
-            //LED1 = ~LED1; 
+            LED1 = ~LED1; 
         }
         i++;
     } 
@@ -294,7 +136,7 @@ void thread2(){
     while(true){//DO Nothing (do only things for testing)
         if(i == 0xFFFE){
             i = 0;
-            //LED2 = ~LED2; 
+            LED2 = ~LED2; 
         }
         i++;
     } 
@@ -304,7 +146,7 @@ void thread3(){
     while(true){//DO Nothing (do only things for testing)
         if(i == 0xFFFE){
             i = 0;
-            //LED3 = ~LED3; 
+            LED3 = ~LED3; 
         }
         i++;
     } 
@@ -314,7 +156,7 @@ void thread4(){
     while(true){//DO Nothing (do only things for testing)
         if(i == 0xFFFE){
             i = 0;
-            //LED4 = ~LED4; 
+            LED4 = ~LED4; 
         }
         i++;
     } 
@@ -324,7 +166,7 @@ void thread5(){
     while(true){//DO Nothing (do only things for testing)
         if(i == 0xFFFE){
             i = 0;
-            //LED5 = ~LED5; 
+            LED5 = ~LED5; 
         }
         i++;
     } 
@@ -334,50 +176,66 @@ void thread6(){
     while(true){//DO Nothing (do only things for testing)
         if(i == 0xFFFE){
             i = 0;
-            //LED6 = ~LED6; 
+            LED6 = ~LED6; 
         }
         i++;
     } 
 }
+
+/**
+ * This is a nice alternative to the other toggle_frontLED function
+ * This function turns on the front_LED, if the selector was set to an even number
+ *
+ * @para[in] PID        identifier of the process to which this function was subscribed
+ * @para[in] eventID    identifier of the event to which this function was subscribed
+ * @para[in] data       the data which was sent with the event
+ * @return   bool       was this function successful?
+ */
 void thread7(){
     uint i = 0;    
     while(true){//DO Nothing (do only things for testing)
         if(i == 0xFFFE){
             i = 0;
-            //LED7 = ~LED7; 
+            LED7 = ~LED7; 
         }
         i++;
     } 
 }
 
-bool wait100times(void *data){
-    static uint8 counter = 0;
-    if(counter++ < 100){
-        return false;
-    }
-    counter = 0;
+/**
+ *
+ * This function toggles the front LED whenever an event occurred to which this function has been subscribed 
+ *
+ * @para[in] PID        identifier of the process to which this function was subscribed
+ * @para[in] eventID    identifier of the event to which this function was subscribed
+ * @para[in] data       the data which was sent with the event
+ * @return   bool       was this function successful?
+ */
+bool toggle_frontLED(uint16 PID, uint16 eventID, sys_event_data *data){
+    FRONT_LED = ~FRONT_LED;
     return true;
 }
 
-bool wait250times(void *data){
-    static uint8 counter = 0;
-    if(counter++ < 250){
-        return false;
+/**
+ * This is a nice alternative to the other toggle_frontLED function
+ * This function turns on the front_LED, if the selector was set to an even number
+ *
+ * @para[in] PID        identifier of the process to which this function was subscribed
+ * @para[in] eventID    identifier of the event to which this function was subscribed
+ * @para[in] data       the data which was sent with the event
+ * @return   bool       was this function successful?
+ */
+bool toggle_frontLED2(uint16 PID, uint16 eventID, sys_event_data *data){
+    uint8 selector_value;
+    if(data->size <= sizeof(uint8)){//does the data fit into an uint32 value
+        selector_value =  *((uint8*) data->value);//store the value
     }
-    counter = 0;
-    return true;
-}
-
-bool wait1000times(void *data){
-    static uint counter = 0;
-    if(counter++ < 1000){
-        return false;
+    
+    if(selector_value % 2 == 0){//is it even=true or odd=false
+       FRONT_LED = 1;
+    }else{
+        FRONT_LED = 0;
     }
-    counter = 0;
-    return true;
-}
-
-bool toggleLED(uint16 PID, uint16 EventID, sys_event_data *data){
-    LED6 = ~LED6;
+    
     return true;
 }
